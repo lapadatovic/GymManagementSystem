db38b1d36258cab1bc19a46d327e1877bd64ad08
Danijel Lapadatovic <l.danijel977@gmail.com>
2023-07-29 23:04:47 +0200

    Created verification for username and password

--- index.php
+++ index.php
@@ -9,15 +9,38 @@
     // Connect to db
     $conn = mysqli_connect($serverName, $dbUsername,$dbPassword, $dbName);
 
-    if($conn){
-        echo "konektovali smo se";
+    if(!$conn){
+        die("Connection failed");
     }
 
     //Check if data are send
     if($_SERVER['REQUEST_METHOD'] == "POST"){
         $username = $_POST["username"];
         $password = $_POST["password"];
+
+        $sql ="SELECT admin_id, password FROM admins WHERE username = ? ";
+        
+        $run = $conn->prepare($sql );
+        $run->bind_param("s", $username);
+        $run->execute();
+        $results = $run->get_result();
+
+        // If its eq with 1 that means we have 1 result
+        if($results->num_rows == 1){
+            
+            // we save fetching results in admin as associative array
+            $admin = $results->fetch_assoc();
+            if($admin['password'] === $password){
+                echo "Password tacan";
+            }else{
+                echo "Password nije tacan";
+            }
+
+        }else{
+            echo 'Admin ne postoji';
+        }
     }
+
 
 ?>
 
